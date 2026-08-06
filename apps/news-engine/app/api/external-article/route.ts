f0aa13578db456d68ccea61e6f8859c7c42fa056
Sunil Kumar <sunil.divyam@gmail.com>
2026-08-06 20:17:40 +0530

    Update route.ts

--- apps/news-engine/app/api/external-article/route.ts
+++ apps/news-engine/app/api/external-article/route.ts
@@ -5,16 +5,21 @@ import { readExternalUrl } from "@worldnews/shared/article-builder/actions/readE
 import { urlHtmlToExternalArticle } from "@worldnews/shared/article-builder/actions/urlHtmlToExternalArticle";
 export const revalidate = 120;
 export async function GET(request: Request) {
+  const errorStage: string[] = [];
   try {
     const { searchParams } = new URL(request.url);
     const url = searchParams.get("url");
     if (!url) return apiError({ message: "Url is required", status: 400 });
-
+    errorStage.push("readExternalUrl");
     const html = await readExternalUrl(url);
+    errorStage.push("urlHtmlToExternalArticle");
     const externalArticle = await urlHtmlToExternalArticle(html, url);
 
     return apiSuccess(externalArticle);
   } catch (err: any) {
+    const str = errorStage.join("=>");
+    console.log(str + err.message);
+    err.message = str + err.message;
     return apiError(err);
   }
 }
